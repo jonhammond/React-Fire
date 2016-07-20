9d320fb3256fc2c31a3afbe30f321006552a541c
Jon Hammond <jonh1016@gmail.com>
2016-07-20 16:54:53 -0600

    Fixed extra <li> glitch

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
 var Header = require('./header');
+var List = require('./list');
 
 var rootUrl = 'https://react-firebase-hammond.firebaseio.com/'
 
@@ -10,21 +11,39 @@ var App = React.createClass({
   // Mixins are a group of methods in an object (ReactFire)
   // that essentially get copy pasted into a component
   mixins: [ ReactFire ],
+  getInitialState: function() {
+    return {
+      items: {},
+      loaded: false
+    };
+  },
   // ComponentWillMount function only runs once
   componentWillMount: function () {
+    fb = new Firebase(rootUrl + 'items/');
     // Bind data onto component from root url to a new
     // object (this.state.items)
-    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    this.bindAsObject(fb, 'items');
+    // When fb emits the 'value' event,
+    // this.handleDataLoaded returns value
+    fb.on('value', this.handleDataLoaded)
   },
   render: function() {
-    return <div className="row panel-default">
+    return <div className="row panel panel-default">
       <div className="col-md-8 col-md-offset-2">
         <h2 className="text-center">
           To-Do List
         </h2>
         <Header itemsStore={this.firebaseRefs.items} />
+        <hr />
+        <div className={"content " + (this.state.loaded ? "loaded" : "")} >
+          <List items={this.state.items} />
+        </div>
       </div>
     </div>
+  },
+  handleDataLoaded: function() {
+    this.setState({
+      loaded: true });
   }
 });
 
