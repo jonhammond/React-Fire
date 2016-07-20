376e90acfb94da64c0f4ab9566f003b0a53160e9
Jon Hammond <jonh1016@gmail.com>
2016-07-19 21:52:14 -0600

    Had to downgrade version of firebase to version 2.06

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
 
-var Hello = React.createClass({
+var rootUrl = 'https://react-firebase-hammond.firebaseio.com/'
+
+var App = React.createClass({
+  // Mixins are a group of methods in an object (ReactFire)
+  // that essentially get copy pasted into a component
+  mixins: [ ReactFire ],
+  // ComponentWillMount function only runs once
+  componentWillMount: function () {
+    // Bind data onto component from root url to a new
+    // object (this.state.items)
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
     return <h1 className="red">
-      Hello!
+      Hello, React!
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
